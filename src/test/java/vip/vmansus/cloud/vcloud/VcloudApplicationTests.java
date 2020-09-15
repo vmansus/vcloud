12fa5a35ed0fd11be8719cd9f9df0ed6a1a45f97
vmansus <cometoit@outlook.com>
2020-09-15 10:49:25 +0800

    first commit

---
+++ src/test/java/vip/vmansus/cloud/vcloud/VcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.vmansus.cloud.vcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
